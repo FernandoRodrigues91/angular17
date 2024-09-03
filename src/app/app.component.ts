f5649198a17567c569a870a32b86d73f23052cdc
Fernando <jk157_372@hotmail.com>
2024-09-03 15:18:38 -0300

    estudo sobre text interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// Components
 import { NewComponentComponent } from './components/new-component/new-component.component';
-
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule,NewComponentComponent],
+  imports: [RouterOutlet, CommonModule,NewComponentComponent, TemplateBindingComponent],
   template: `
     <!--<router-outlet></router-outlet>-->
+    <!--app-new-component/>-->
     <h1>Curso de Angular 17</h1>
-    <div class="theme-red">
-      <app-new-component />
+    <app-template-binding/>
 
-    </div>
 
 
   `
---
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-binding',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './template-binding.component.html',
+  styleUrl: './template-binding.component.scss'
+})
+export class TemplateBindingComponent {
+  public name = "Fernando Rodrigues";
+  public age = 32;
+
+  public sum(val1: number,val2 : number){
+    return val1 + val2;
+  }
+
+}
