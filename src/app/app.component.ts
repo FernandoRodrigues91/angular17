cac0318f1f2e3d454b1a4f12a9b9e12812c96038
fernando junior <jk157_372@hotmail.com>
2025-01-02 17:43:07 -0300

    estudo de control  flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,25 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponentComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from "./components/template/template-variables/template-variables.component";
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NewComponentComponent, TemplateBindingComponent, TemplateVariablesComponent],
+  imports: [RouterOutlet, 
+    CommonModule, 
+    NewComponentComponent, 
+    TemplateBindingComponent, 
+    TemplateVariablesComponent, 
+    TemplateControlFlowComponent
+  ],
   template: `
     <!--<router-outlet></router-outlet>-->
     <!--app-new-component/>-->
     <h1>Curso de Angular 17</h1>
-    <app-template-binding/>
-    <app-template-variables />
+    <!-- <app-template-binding/> -->
+    <!-- <app-template-variables /> -->
+     
+     <app-template-control-flow />
 
 
 
