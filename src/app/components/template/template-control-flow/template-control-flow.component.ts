63cb567ccfd4dbc31efe7ec43f5b34a97c53e559
Fernando Rodrigues <jk157_372@hotmail.com>
2025-01-19 18:48:02 -0300

    estudo e exercicio de FOR e seis valores

--- src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -1,17 +1,19 @@
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { delay, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [CommonModule, AsyncPipe, ],
+  imports: [CommonModule, AsyncPipe, NgFor ],
   templateUrl: './template-control-flow.component.html',
   styleUrl: './template-control-flow.component.scss'
 })
 export class TemplateControlFlowComponent {
 
   public isTrue = true; 
+
+  public itens = [{name: 'Fernando Rodrigues'}];
 
   public loadingData$: Observable<string[]> = of([
     'item 1 ',
@@ -21,6 +23,10 @@ export class TemplateControlFlowComponent {
 
   public trackByFn(index: number){
     return index;
+  }
+
+  public addNewName(value: string){
+    return this.itens.push({ name: value});
   }
   
 }
