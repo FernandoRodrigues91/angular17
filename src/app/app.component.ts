6e828b1ef0a55a3646384ff596b4d26cac01797b
Fernando Rodrigues <jk157_372@hotmail.com>
2025-01-30 10:52:09 -0300

    iniciado estudo aprofundado sobre Ferreable Views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { NewComponentComponent } from './components/new-component/new-component.
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from "./components/template/template-variables/template-variables.component";
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,16 +15,18 @@ import { TemplateControlFlowComponent } from './components/template/template-con
     NewComponentComponent, 
     TemplateBindingComponent, 
     TemplateVariablesComponent, 
-    TemplateControlFlowComponent
+    TemplateControlFlowComponent,
+    TemplateDeferrableViewsComponent
   ],
   template: `
     <!--<router-outlet></router-outlet>-->
     <!--app-new-component/>-->
-    <h1>Curso de Angular 17</h1>
     <!-- <app-template-binding/> -->
     <!-- <app-template-variables /> -->
-     
-     <app-template-control-flow />
+    <!-- <app-template-control-flow /> -->
+    <h1>Curso de Angular 17</h1>
+    <app-template-deferrable-views/>
+
 
 
 
